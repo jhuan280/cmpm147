b3a396d279cd73567c545244a440a7f501673806
jhuan280 <92900160+jhuan280@users.noreply.github.com>
2024-04-30 21:18:36 -0700

    Pages world2
    updated world2 on the pages. should show up on the same canvas as world1

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -7,6 +7,9 @@
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
+
+//world1//
+//#--------------------------------------------------------------------------------------------------------------------------#//
 "use strict";
 
 const w1 = (sketch) => {
@@ -238,7 +241,7 @@ const w1 = (sketch) => {
   
   sketch.mouseClicked = () => {
     let world_pos = screenToWorld(
-      [0 - mouseX, mouseY],
+      [0 - sketch.mouseX, sketch.mouseY],
       [camera_offset.x, camera_offset.y]
     );
   
@@ -351,6 +354,362 @@ const w1 = (sketch) => {
 
 let world1 = new p5(w1, 'w1');
 
+//world1//
+//#--------------------------------------------------------------------------------------------------------------------------#//
+
+
+//world2//
+//#--------------------------------------------------------------------------------------------------------------------------#//
+const w2 = (sketch) => {
+
+  /* global XXH */
+  /* exported --
+      p3_preload
+      p3_setup
+      p3_worldKeyChanged
+      p3_tileWidth
+      p3_tileHeight
+      p3_tileClicked
+      p3_drawBefore
+      p3_drawTile
+      p3_drawSelectedTile
+      p3_drawAfter
+  */
+  
+  let shrub;
+  let rock;
+  let water;
+  let grass;
+  let flower;
+  let raindrops = [];
+  
+  function p3_preload() {
+    shrub = sketch.loadImage('https://cdn.glitch.global/c9e28603-a30e-48a2-88aa-71aacb622551/tile_032.png?v=1714453107197'); //32
+    rock = sketch.loadImage('https://cdn.glitch.global/c9e28603-a30e-48a2-88aa-71aacb622551/tile_067.png?v=1714453112704'); //67
+    water = sketch.loadImage('https://cdn.glitch.global/c9e28603-a30e-48a2-88aa-71aacb622551/tile_104.png?v=1714467293272')
+    grass = sketch.loadImage('https://cdn.glitch.global/c9e28603-a30e-48a2-88aa-71aacb622551/tile_037.png?v=1714453127412'); //37
+    flower = sketch.loadImage('https://cdn.glitch.global/c9e28603-a30e-48a2-88aa-71aacb622551/tile_044.png?v=1714464956976')
+  }
+  
+  function p3_setup() {
+    // Initialize raindrops
+    for (let i = 0; i < 500; i++) {
+      raindrops.push({
+        x: sketch.random(sketch.width / 2, sketch.width), // Start from the top right corner
+        y: sketch.random(0, sketch.height / 2), // Randomize vertical position within the top half of the canvas
+        speed: sketch.random(2, 5) // Random speed
+      });
+    }
+  }
+  
+  let worldSeed;
+  
+  function p3_worldKeyChanged(key) {
+    worldSeed = XXH.h32(key, 0);
+    sketch.noiseSeed(worldSeed);
+    sketch.randomSeed(worldSeed);
+  }
+  
+  function p3_tileWidth() {
+    return 32;
+  }
+  
+  function p3_tileHeight() {
+    return 16;
+  }
+  
+  let [tw, th] = [p3_tileWidth(), p3_tileHeight()];
+  
+  let clicks = {};
+  
+  function p3_tileClicked(i, j) {
+    let key = [i, j];
+    clicks[key] = 1 + (clicks[key] | 0);
+  }
+  
+  function p3_drawBefore() {
+    // Clear the background
+    sketch.background(255);
+  }
+  
+  function p3_drawTile(i, j) {
+    sketch.noStroke();
+  
+    if ((sketch.floor(7 * sketch.noise(i, j))) < 2) {
+      sketch.image(grass, 0, 0, 64, 64); // Display grass
+    } else if ((sketch.floor(2 * sketch.noise(i, j))) < 1) {
+      let imgSize = 64; // Size of each tile in the grid
+      sketch.image(water, 0, 0, imgSize, imgSize); // Display water at top-left corner
+      sketch.image(water, imgSize, 0, imgSize, imgSize); // Display water at top-right corner
+      sketch.image(water, 0, imgSize, imgSize, imgSize); // Display water at bottom-left corner
+      sketch.image(water, imgSize, imgSize, imgSize, imgSize); // Display water at bottom-right corner
+    } 
+    else if ((sketch.floor(4 * sketch.noise(i, j))) < 3) {
+      sketch.image(shrub, 0, 0, 64, 64); // Display shrub
+    } 
+    else if ((sketch.floor(3 * sketch.noise(i, j))) < 4) {
+      sketch.image(flower, 0, 0, 64, 64); // Display flower
+    }
+  
+    if (XXH.h32("tile:" + [i, j], worldSeed) % 8 == 0) {
+      sketch.image(rock, 0, 0, 64, 64); // Display rock
+    }
+  
+    // Your additional code (if any) can go here
+  
+    sketch.push();
+    // Your additional drawing code (if any) can go here
+    sketch.pop();
+  }
+  
+  function p3_drawSelectedTile(i, j) {
+    sketch.noFill();
+    sketch.stroke(0, 255, 0, 128);
+  
+    sketch.beginShape();
+    sketch.vertex(-tw, 0);
+    sketch.vertex(0, th);
+    sketch.vertex(tw, 0);
+    sketch.vertex(0, -th);
+    sketch.endShape(sketch.CLOSE);
+  
+    sketch.noStroke();
+    sketch.fill(0);
+    sketch.text("tile " + [i, j], 0, 0);
+  }
+  
+  function p3_drawAfter() {
+    // Draw raindrops falling diagonally from top right to bottom left
+    for (let i = 0; i < raindrops.length; i++) {
+      let drop = raindrops[i];
+      drop.x -= drop.speed; // Move the raindrop towards the left
+      drop.y += drop.speed; // Move the raindrop downwards
+      sketch.stroke(0, 0, 255); // Blue color for raindrops
+      sketch.line(drop.x, drop.y, drop.x + 5, drop.y + 5); // Draw raindrop
+      // If the raindrop moves off the screen, reset its position
+      if (drop.x < 0 || drop.y > sketch.height) {
+        drop.x = sketch.random(sketch.width / 2, sketch.width); // Start from the top right corner again
+        drop.y = sketch.random(0, sketch.height / 2); // Randomize vertical position within the top half of the canvas
+      }
+    }
+  }
+  
+
+/* global p5 */
+/* exported preload, setup, draw, mouseClicked */
+
+// Project base code provided by {amsmith,ikarth}@ucsc.edu
+
+
+let tile_width_step_main; // A width step is half a tile's width
+let tile_height_step_main; // A height step is half a tile's height
+
+// Global variables. These will mostly be overwritten in setup().
+let tile_rows, tile_columns;
+let camera_offset;
+let camera_velocity;
+
+/////////////////////////////
+// Transforms between coordinate systems
+// These are actually slightly weirder than in full 3d...
+/////////////////////////////
+function worldToScreen([world_x, world_y], [camera_x, camera_y]) {
+  let i = (world_x - world_y) * tile_width_step_main;
+  let j = (world_x + world_y) * tile_height_step_main;
+  return [i + camera_x, j + camera_y];
+}
+
+function worldToCamera([world_x, world_y], [camera_x, camera_y]) {
+  let i = (world_x - world_y) * tile_width_step_main;
+  let j = (world_x + world_y) * tile_height_step_main;
+  return [i, j];
+}
+
+function tileRenderingOrder(offset) {
+  return [offset[1] - offset[0], offset[0] + offset[1]];
+}
+
+function screenToWorld([screen_x, screen_y], [camera_x, camera_y]) {
+  screen_x -= camera_x;
+  screen_y -= camera_y;
+  screen_x /= tile_width_step_main * 2;
+  screen_y /= tile_height_step_main * 2;
+  screen_y += 0.5;
+  return [Math.floor(screen_y + screen_x), Math.floor(screen_y - screen_x)];
+}
+
+function cameraToWorldOffset([camera_x, camera_y]) {
+  let world_x = camera_x / (tile_width_step_main * 2);
+  let world_y = camera_y / (tile_height_step_main * 2);
+  return { x: Math.round(world_x), y: Math.round(world_y) };
+}
+
+function worldOffsetToCamera([world_x, world_y]) {
+  let camera_x = world_x * (tile_width_step_main * 2);
+  let camera_y = world_y * (tile_height_step_main * 2);
+  return new p5.Vector(camera_x, camera_y);
+}
+
+sketch.preload = () => {
+  // if (window.p3_preload) {
+  p3_preload();
+  // }
+}
+
+sketch.setup = () => {
+  let canvas = sketch.createCanvas(800, 400);
+  canvas.parent("container2");
+
+  camera_offset = new p5.Vector(-sketch.width / 2, sketch.height / 2);
+  camera_velocity = new p5.Vector(0, 0);
+
+  // if (window.p3_setup) {
+  p3_setup();
+  // }
+
+  let label = sketch.createP();
+  label.html("World key: ");
+  label.parent("container2");
+
+  let input = sketch.createInput("xyzzy");
+  input.parent(label);
+  input.input(() => {
+    rebuildWorld(input.value());
+  });
+
+  sketch.createP("Arrow keys scroll. Clicking changes tiles.").parent("container2");
+
+  rebuildWorld(input.value());
+}
+
+function rebuildWorld(key) {
+  // if (window.p3_worldKeyChanged) {
+  p3_worldKeyChanged(key);
+  // }
+  tile_width_step_main = window.p3_tileWidth ? p3_tileWidth() : 32;
+  tile_height_step_main = window.p3_tileHeight ? p3_tileHeight() : 14.5;
+  tile_columns = Math.ceil(sketch.width / (tile_width_step_main * 2));
+  tile_rows = Math.ceil(sketch.height / (tile_height_step_main * 2));
+}
+
+sketch.mouseClicked = () => {
+  let world_pos = screenToWorld(
+    [0 - sketch.mouseX, sketch.mouseY],
+    [camera_offset.x, camera_offset.y]
+  );
+
+  // if (window.p3_tileClicked) {
+  p3_tileClicked(world_pos[0], world_pos[1]);
+  // }
+  return false;
+}
+
+sketch.draw = () => {
+  // Keyboard controls!
+  if (sketch.keyIsDown(sketch.LEFT_ARROW)) {
+    camera_velocity.x -= 1;
+  }
+  if (sketch.keyIsDown(sketch.RIGHT_ARROW)) {
+    camera_velocity.x += 1;
+  }
+  if (sketch.keyIsDown(sketch.DOWN_ARROW)) {
+    camera_velocity.y -= 1;
+  }
+  if (sketch.keyIsDown(sketch.UP_ARROW)) {
+    camera_velocity.y += 1;
+  }
+
+  let camera_delta = new p5.Vector(0, 0);
+  camera_velocity.add(camera_delta);
+  camera_offset.add(camera_velocity);
+  camera_velocity.mult(0.95); // cheap easing
+  if (camera_velocity.mag() < 0.01) {
+    camera_velocity.setMag(0);
+  }
+
+  let world_pos = screenToWorld(
+    [0 - sketch.mouseX, sketch.mouseY],
+    [camera_offset.x, camera_offset.y]
+  );
+  let world_offset = cameraToWorldOffset([camera_offset.x, camera_offset.y]);
+
+  sketch.background(100);
+
+  // if (window.p3_drawBefore) {
+  p3_drawBefore();
+  // }
+
+  let overdraw = 0.1;
+
+  let y0 = Math.floor((0 - overdraw) * tile_rows);
+  let y1 = Math.floor((1 + overdraw) * tile_rows);
+  let x0 = Math.floor((0 - overdraw) * tile_columns);
+  let x1 = Math.floor((1 + overdraw) * tile_columns);
+
+  for (let y = y0; y < y1; y++) {
+    for (let x = x0; x < x1; x++) {
+      drawTile(tileRenderingOrder([x + world_offset.x, y - world_offset.y]), [
+        camera_offset.x,
+        camera_offset.y
+      ]); // odd row
+    }
+    for (let x = x0; x < x1; x++) {
+      drawTile(
+        tileRenderingOrder([
+          x + 0.5 + world_offset.x,
+          y + 0.5 - world_offset.y
+        ]),
+        [camera_offset.x, camera_offset.y]
+      ); // even rows are offset horizontally
+    }
+  }
+
+  describeMouseTile(world_pos, [camera_offset.x, camera_offset.y]);
+
+  // if (window.p3_drawAfter) {
+  p3_drawAfter();
+  // }
+}
+
+// Display a discription of the tile at world_x, world_y.
+function describeMouseTile([world_x, world_y], [camera_x, camera_y]) {
+  let [screen_x, screen_y] = worldToScreen(
+    [world_x, world_y],
+    [camera_x, camera_y]
+  );
+  drawTileDescription([world_x, world_y], [0 - screen_x, screen_y]);
+}
+
+function drawTileDescription([world_x, world_y], [screen_x, screen_y]) {
+  sketch.push();
+  sketch.translate(screen_x, screen_y);
+  // if (window.p3_drawSelectedTile) {
+  p3_drawSelectedTile(world_x, world_y, screen_x, screen_y);
+  // }
+  sketch.pop();
+}
+
+// Draw a tile, mostly by calling the user's drawing code.
+function drawTile([world_x, world_y], [camera_x, camera_y]) {
+  let [screen_x, screen_y] = worldToScreen(
+    [world_x, world_y],
+    [camera_x, camera_y]
+  );
+  sketch.push();
+  sketch.translate(0 - screen_x, screen_y);
+  // if (window.p3_drawTile) {
+  p3_drawTile(world_x, world_y, -screen_x, screen_y);
+  // }
+  sketch.pop();
+}
+
+
+}
+
+let world2 = new p5(w2, 'w2');
+
+//world1//
+//#--------------------------------------------------------------------------------------------------------------------------#//
 
 
 
